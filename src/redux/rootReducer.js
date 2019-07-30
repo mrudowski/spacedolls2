38f232da9e21dd318f16920f86b7693736e719e0
mrudowski <45997056+mrudowski@users.noreply.github.com>
2019-07-30 17:30:52 +0200

    action attack 2

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,13 +3,15 @@ import counter from './reducers/counter';
 import level from './level';
 import board from './board';
 import dolls from './dolls';
+import actions from './actions';
 
 // rootReducer
 export default combineReducers({
   counter,
   level: level.reducer,
   dolls: dolls.reducer,
-  board: board.reducer
+  board: board.reducer,
+  actions: actions.reducer
 });
 
 // export default (state = {}, action) => {
